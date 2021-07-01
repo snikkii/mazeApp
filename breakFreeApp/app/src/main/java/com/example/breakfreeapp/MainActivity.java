4e33dcb80409150c7cb7ec9f08ae3098ea4de2c0
annika <anistadelmann@t-online.de>
2021-07-01 14:49:41 +0200

    save button now in emulator& log raus aus main

--- breakFreeApp/app/src/main/java/com/example/breakfreeapp/MainActivity.java
+++ breakFreeApp/app/src/main/java/com/example/breakfreeapp/MainActivity.java
@@ -198,7 +198,6 @@ public class MainActivity extends AppCompatActivity {
             String validIp = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
             Pattern pattern = Pattern.compile(validIp);
             Matcher matcher = pattern.matcher(BROKER);
-            Log.d("MAIN", String.valueOf(matcher.matches()));
             if(!matcher.matches()){
                 raspberrySwitchOn = false;
                 String brokerBuffer = BROKER;
